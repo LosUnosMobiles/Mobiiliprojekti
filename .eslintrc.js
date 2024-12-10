6ab32ab9d900d9a3e72f73bbfacb4d39346c9e43
Mikko Kujala <m.kujala@live.com>
2024-12-10 14:56:06 +0200

    add eslint-plugin-react as dev dependency and edit .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,29 @@
 module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended" // If using React
+    ],
     overrides: [
         {
             // Test files only
-            files: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-            extends: ['plugin:testing-library/react'],
+            files: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+            extends: ["plugin:testing-library/react"],
         },
     ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: [
+        "react" // If using React
+    ],
+    rules: {
+        "quotes": ["error", "double"], // Enforce double quotes
+        "semi": "off"
+    }
 };
